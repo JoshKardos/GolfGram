328f843a565f56cb1eeb84b089886adb36bedb96
Josh Kardos <joshkardos@joshs-macbook-pro.local>
2018-09-25 17:24:10 -0700

    Added View Controllers and Chameleon Framework

--- GolfGram/AppDelegate.swift
+++ GolfGram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import ChameleonFramework
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
+		
+		//change color of tab bar items
+		UITabBar.appearance().tintColor = UIColor.flatGreenDark
+		
+
 		return true
 	}
 
