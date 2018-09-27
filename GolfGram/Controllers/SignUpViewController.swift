754fa2ada7d59fd80204530f739f41a89eddef1c
Josh Kardos <joshkardos@joshs-macbook-pro.local>
2018-09-26 23:31:07 -0700

    Commented SignUpVC

--- GolfGram/Controllers/SignUpViewController.swift
+++ GolfGram/Controllers/SignUpViewController.swift
@@ -12,13 +12,14 @@ import FirebaseDatabase
 import FirebaseStorage
 
 class SignUpViewController: UIViewController {
+	
 	@IBOutlet weak var usernameTextField: UITextField!
 	@IBOutlet weak var emailTextField: UITextField!
 	@IBOutlet weak var passwordTextField: UITextField!
 	@IBOutlet weak var profileImage: UIImageView!
 	
 	var selectedImage : UIImage?
-	var imageURL:URL?
+	//var imageURL:URL?
 	
 	override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,39 +52,46 @@ class SignUpViewController: UIViewController {
 	
 	@IBAction func signUpPressed(_ sender: Any) {
 		
-		
+		/////////////////////
+		////Create User//////
+		/////////////////////
 		Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
-			
 			if error != nil {
 				print("Eror creating user: \(error!.localizedDescription)")
 				return
 			}
+		
+			//Get reference to the profile images//
+			///////////////////////////////////////
+			let storageRef = Storage.storage().reference(forURL: "gs://golfgram-68599.appspot.com").child("profile_image").child((user?.user.uid)!)
 			
-			let uid = user?.user.uid
-			
-			let storageRef = Storage.storage().reference(forURL: "gs://golfgram-68599.appspot.com").child("profile_image").child(uid!)
-			
+			//Must have selected an image
 			if let profileImg = self.selectedImage{
 				print("Inside")
+				
+				//turn sleected photo into jpeg
 				guard let imageData = profileImg.jpegData(compressionQuality: 0.1) else { fatalError("ERROR INSIDE")}
 				
+				//insert jpeg data into database with the url
 				storageRef.putData(imageData, metadata: nil, completion: { (metadata, error) in
 					if error != nil{
-						print("AYO ERROR")
+						print("Error \(error?.localizedDescription)")
 						return
 					}
-					
 					storageRef.downloadURL { (url, error) in
-						
+						if error != nil{
+							print("Error \(error?.localizedDescription)")
+							return
+						}
+						//get url of image
 						guard let profileImageUrl = url?.absoluteString else {return}
 						
+						//get referenece to users in the database
+						let usersRef = Database.database().reference().child("users")
 						
-						let ref = Database.database().reference()
-						let usersReference = ref.child("users")
 						
-						
-						usersReference.childByAutoId().setValue(["username": self.usernameTextField.text!, "email" : self.emailTextField.text!, "profileImageUrl": profileImageUrl])
-					
+						//save user(username, email, profileImage)
+						usersRef.childByAutoId().setValue(["username": self.usernameTextField.text!, "email" : self.emailTextField.text!, "profileImageUrl": profileImageUrl])
 					}
 				})
 			}
