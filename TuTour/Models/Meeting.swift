ad3a69c1ad861c9c115024101ea8826f539b6e67
Josh Kardos <joshkardos@joshs-macbook-pro.local>
2019-01-24 18:35:53 -0800

    commit - JK

---
+++ TuTour/Models/Meeting.swift
@@ -0,0 +1,31 @@
+//
+//  Meeting.swift
+//  GolfGram
+//
+//  Created by Josh Kardos on 1/19/19.
+//  Copyright © 2019 JoshTaylorKardos. All rights reserved.
+//
+
+import Foundation
+class MeetingRequest{
+	
+	var time: Date?
+	var location: String?
+	var tutorUid: String?
+	var tutoreeUid: String?
+	var subject: String?
+	
+	
+	func setLocation(location: String){
+		self.location = location
+	}
+	func setDate(date: Date){
+		self.time = date
+	}
+	
+	init(tutor: String, tutoree: String, subject: String) {
+		self.tutorUid = tutor
+		self.tutoreeUid = tutoree
+		self.subject = subject
+	}
+}
