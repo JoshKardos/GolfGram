1529f97ea2a1b79c24d805ef32e10b070de20f7a
gideon ubaldo <gideon.ubaldo@sjsu.edu>
2019-02-05 17:38:19 -0800

    profile Aesthethic update

--- TuTour/Controllers/ProfileViewController.swift
+++ TuTour/Controllers/ProfileViewController.swift
@@ -9,101 +9,112 @@ import UIKit
 import FirebaseAuth
 import FirebaseDatabase
 class ProfileViewController: UIViewController {
-	
-	@IBOutlet weak var profileImage: UIImageView!
-	@IBOutlet weak var usernameLabel: UILabel!
-	@IBOutlet weak var emailLabel: UILabel!
-	@IBOutlet weak var dmButton: UIBarButtonItem!
-	@IBOutlet weak var registerAsTutorButton: UIButton!
-	
-	var isOtherUser = false//must change to false if vc is pushed programatically!!
-	
-	var uid: String?
-	
-	var meeting: MeetingRequest?
-	
-	@IBAction func tutorButton(_ sender: Any) {
-		
-		if isOtherUser == false{//is current users profile
-			
-			let storyboard: UIStoryboard = UIStoryboard(name: "Profile", bundle: nil)
-			let subjectsVC = storyboard.instantiateViewController(withIdentifier: "Subjects") as! SubjectsViewController
-			
-			navigationController?.pushViewController(subjectsVC, animated: true)
-			print("HERE" )
-			
-		} else{//is other users profile
-			selectMeetingOptions()
-			print("ELSE")
-			
-		}
-	}
-	
-	func selectMeetingOptions(){
-		
-		let storyboard: UIStoryboard = UIStoryboard(name: "Profile", bundle: nil)
-		let timeOptionsVC = storyboard.instantiateViewController(withIdentifier: "TimeOptions") as! DatePickerViewController
-		
-		timeOptionsVC.meeting = meeting
-		navigationController?.pushViewController(timeOptionsVC, animated: true)
-		
-		
-	}
-	
-	func disableComponents(){
-		dmButton.isEnabled = false
-		dmButton.image = nil
-		dmButton.title = nil
-	}
-	
-	func fillUserInfo(uid: String){
-		
-		
-		Database.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: {(snapshot) in
-			
-			let username = (snapshot.value as! NSDictionary)["username"] as! String
-			self.usernameLabel?.text = username
-			
-			
-			let profileImageString = (snapshot.value as! NSDictionary)["profileImageUrl"] as! String
-			let url = URL(string: profileImageString)
-			let imageData = NSData.init(contentsOf: url as! URL)
-			self.profileImage?.image = UIImage(data: imageData as! Data)
-			
-			
-			let emailString = (snapshot.value as! NSDictionary)["email"] as! String
-			self.emailLabel?.text = emailString
-			
-		})
-		
-	}
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		
-		dmButton?.tintColor = UIColor.flatGreenDark
+    
+    @IBOutlet weak var emailTextbox: UITextField!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var dmButton: UIBarButtonItem!
+    @IBOutlet weak var registerAsTutorButton: UIButton!
+    @IBAction func touch_email(_ sender: UIButton) {
+    }
+    @IBAction func touch_year(_ sender: UIButton) {
+        
+    }
+    @IBAction func touch_rating(_ sender: UIButton) {
+        
+    }
+    @IBAction func touch_moreinfo(_ sender: UIButton) {
+        
+    }
+    
+    var isOtherUser = false//must change to false if vc is pushed programatically!!
+    
+    var uid: String?
+    
+    var meeting: MeetingRequest?
+    
+    @IBAction func tutorButton(_ sender: Any) {
+        
+        if isOtherUser == false{//is current users profile
+            
+            let storyboard: UIStoryboard = UIStoryboard(name: "Profile", bundle: nil)
+            let subjectsVC = storyboard.instantiateViewController(withIdentifier: "Subjects") as! SubjectsViewController
+            
+            navigationController?.pushViewController(subjectsVC, animated: true)
+            print("HERE" )
+            
+        } else{//is other users profile
+            selectMeetingOptions()
+            print("ELSE")
+            
+        }
+    }
+    
+    func selectMeetingOptions(){
+        
+        let storyboard: UIStoryboard = UIStoryboard(name: "Profile", bundle: nil)
+        let timeOptionsVC = storyboard.instantiateViewController(withIdentifier: "TimeOptions") as! DatePickerViewController
+        
+        timeOptionsVC.meeting = meeting
+        navigationController?.pushViewController(timeOptionsVC, animated: true)
+        
+        
+    }
+    
+    func disableComponents(){
+        dmButton.isEnabled = false
+        dmButton.image = nil
+        dmButton.title = nil
+    }
+    
+    func fillUserInfo(uid: String){
+        
+        
+        Database.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: {(snapshot) in
+            
+            let username = (snapshot.value as! NSDictionary)["username"] as! String
+            self.usernameLabel?.text = username
+            
+            
+            let profileImageString = (snapshot.value as! NSDictionary)["profileImageUrl"] as! String
+            let url = URL(string: profileImageString)
+            let imageData = NSData.init(contentsOf: url as! URL)
+            self.profileImage?.image = UIImage(data: imageData as! Data)
+            
+            
+            let emailString = (snapshot.value as! NSDictionary)["email"] as! String
+            self.emailTextbox.text = emailString
+        })
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dmButton?.tintColor = UIColor.flatGreenDark
         
         // Converts the profile image to circular
-//        profileImage.layer.borderWidth = 1
-//        profileImage.layer.masksToBounds = false
-//        profileImage.layer.borderColor = UIColor.gray.cgColor
-//        profileImage.layer.cornerRadius = profileImage.frame.height/2
-//        profileImage.clipsToBounds = true
+        //        profileImage.layer.borderWidth = 1
+        //        profileImage.layer.masksToBounds = false
+        //        profileImage.layer.borderColor = UIColor.gray.cgColor
+        //        profileImage.layer.cornerRadius = profileImage.frame.height/2
+        //        profileImage.clipsToBounds = true
         
-		if let otherUser_ID = self.uid {//other users pprofile
-			
-			self.isOtherUser = true
-			fillUserInfo(uid: otherUser_ID)
-			registerAsTutorButton.backgroundColor = UIColor.yellow
-			registerAsTutorButton.setTitle("Tutor Request", for: .normal)
-			
-		} else {//current users profile
-			
-			let userID : String = (Auth.auth().currentUser?.uid)!
-			fillUserInfo(uid: userID)
-			isOtherUser = false
-			registerAsTutorButton.reloadInputViews()
-			
-		}
-	}
+        if let otherUser_ID = self.uid {//other users pprofile
+            
+            self.isOtherUser = true
+            fillUserInfo(uid: otherUser_ID)
+            registerAsTutorButton.backgroundColor = UIColor.yellow
+            registerAsTutorButton.setTitle("Tutor Request", for: .normal)
+            
+        } else {//current users profile
+            
+            let userID : String = (Auth.auth().currentUser?.uid)!
+            fillUserInfo(uid: userID)
+            isOtherUser = false
+            registerAsTutorButton.reloadInputViews()
+            
+        }
+    }
 }
