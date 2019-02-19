245ad0cc3fcd886c99ad49cc05c15057b1809463
Oobnala <alnboo1@gmail.com>
2019-02-18 19:46:37 -0800

    UIPicker now working. Isao style not being used since incompatible with TextFieldEffects

--- TuTour/Controllers/ProfileSettingsController.swift
+++ TuTour/Controllers/ProfileSettingsController.swift
@@ -11,7 +11,7 @@ import FirebaseAuth
 import FirebaseDatabase
 import TextFieldEffects
 
-class ProfileSettingsController: UIViewController {
+class ProfileSettingsController: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
   
     
     @IBOutlet weak var updateButton: UIButton!
@@ -22,60 +22,142 @@ class ProfileSettingsController: UIViewController {
     @IBOutlet weak var yearField: UITextField!
     @IBOutlet weak var descriptionField: UITextField!
     
+    
+    var currentTextField = UITextField()
+    var pickerView = UIPickerView()
+    
+//    var nameFieldIsao: IsaoTextField!
+//    var schoolFieldIsao: IsaoTextField!
+//    var majorFieldIsao: IsaoTextField!
+//    var yearFieldIsao: IsaoTextField!
+//    var descFieldIsao: IsaoTextField!
+    
+    let schoolArray = ["SJSU", "UCSD", "UCLA"]
+    let majorArray = ["Engineering", "English", "Media"]
+    let yearArray = ["19", "20", "21", "22"]
+    
+    
     @IBAction func confirmAction(_ sender: Any) {
         
         
-        
-        
     }
-    func getData(uid: String) {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if currentTextField == schoolField {
+            return schoolArray.count
+        } else if currentTextField == majorField {
+            return majorArray.count
+        } else if currentTextField == yearField {
+            return yearArray.count
+        } else {
+            return 0
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if currentTextField == schoolField {
+            return schoolArray[row]
+        } else if currentTextField == majorField {
+            return majorArray[row]
+        } else if currentTextField == yearField {
+            return yearArray[row]
+        } else {
+            return ""
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if currentTextField == schoolField {
+            self.schoolField.text = schoolArray[row]
+            self.view.endEditing(true)
+        } else if currentTextField == majorField {
+            self.majorField.text = majorArray[row]
+            self.view.endEditing(true)
+        } else if currentTextField == yearField {
+            self.yearField.text = yearArray[row]
+            self.view.endEditing(true)
+        }
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.pickerView.delegate = self
+        self.pickerView.dataSource = self
         
+        currentTextField = textField
+        
+        if currentTextField == schoolField {
+            print("SCHOOOOOL")
+            currentTextField.inputView = pickerView
+        } else if currentTextField == majorField {
+           currentTextField.inputView = pickerView
+        } else if currentTextField == yearField {
+            currentTextField.inputView = pickerView
+        }
+    }
+    
+    func getData(uid: String) {
+    
         Database.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: {(snapshot) in
             
-            let profileImageString = (snapshot.value as! NSDictionary)["profileImageUrl"] as! String
-            let url = URL(string: profileImageString)
-            let imageData = NSData.init(contentsOf: url as! URL)
-            self.profilePhoto?.image = UIImage(data: imageData as! Data)
+                let profileImageString = (snapshot.value as! NSDictionary)["profileImageUrl"] as! String
+                let url = URL(string: profileImageString)
+                let imageData = NSData.init(contentsOf: url as! URL!)
+                self.profilePhoto?.image = UIImage(data: imageData as! Data!)
             
-        })
+                let nameString = ((snapshot.value as! NSDictionary)["fullname"] as! String)
+                self.nameField.text = nameString
+                let schoolString = ((snapshot.value as! NSDictionary)["school"] as! String)
+                self.schoolField.text = schoolString
+                let majorString = ((snapshot.value as! NSDictionary)["major"] as! String)
+                self.majorField.text = majorString
+                let yearString = ((snapshot.value as! NSDictionary)["year"] as! String)
+                self.yearField.text = yearString
+            })
         
     }
     
-    func styleFields(field: UITextField) -> IsaoTextField {
-        
-        let styledField = IsaoTextField(frame: field.frame)
-        //styledField.placeholderColor = .lightGray
-        styledField.activeColor = UIColor(red: 0/255, green: 106/255, blue: 255/255, alpha: 1.0)
-        styledField.inactiveColor = .lightGray
-        
-        return styledField
-    }
+    // Convert UITextFields to Isao
+//    func styleFields(field: UITextField) -> IsaoTextField {
+//
+//        let styledField = IsaoTextField(frame: field.frame)
+//        styledField.activeColor = UIColor(red: 0/255, green: 106/255, blue: 255/255, alpha: 1.0)
+//        styledField.inactiveColor = .lightGray
+//
+//        return styledField
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let userID = Auth.auth().currentUser!.uid
+        
+//        self.nameFieldIsao = styleFields(field: nameField)
+//        nameFieldIsao.placeholder = "Name"
+//
+//        self.schoolFieldIsao = styleFields(field: schoolField)
+//        schoolFieldIsao.placeholder = "School"
+//
+//        self.majorFieldIsao = styleFields(field: majorField)
+//        majorFieldIsao.placeholder = "Major"
+//
+//        self.yearFieldIsao = styleFields(field: yearField)
+//        yearFieldIsao.placeholder = "Year"
+//
+//        self.descFieldIsao = styleFields(field: descriptionField)
+//        descFieldIsao.placeholder = "Description"
         
         getData(uid: userID)
         
-        
-        let nameFieldIsao = styleFields(field: nameField)
-        nameFieldIsao.placeholder = "Name"
-        let schoolFieldIsao = styleFields(field: schoolField)
-        schoolFieldIsao.placeholder = "School"
-        let majorFieldIsao = styleFields(field: majorField)
-        majorFieldIsao.placeholder = "Major"
-        let yearFieldIsao = styleFields(field: yearField)
-        yearFieldIsao.placeholder = "Year"
-        let descriptionFieldIsao = styleFields(field: descriptionField)
-        descriptionFieldIsao.placeholder = "Description"
-
-        
-        self.view.addSubview(nameFieldIsao)
-        self.view.addSubview(schoolFieldIsao)
-        self.view.addSubview(majorFieldIsao)
-        self.view.addSubview(yearFieldIsao)
-        self.view.addSubview(descriptionFieldIsao)
+        //self.view.addSubview(nameFieldIsao)
+        //self.view.addSubview(schoolFieldIsao)
+        //self.view.addSubview(majorFieldIsao)
+        //self.view.addSubview(yearFieldIsao)
+        //self.view.addSubview(descFieldIsao)
+        //self.view.addSubview(pickerView)
     }
     
 
