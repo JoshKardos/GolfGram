13712becb54589a5354ef37d9a2e47c50851b362
Josh <joshkardos@gmail.com>
2019-02-27 11:50:46 -0800

    newsfeed design complete, no logic has been done - JK

--- TuTour/Cells/PostCellViewController.swift
+++ TuTour/Cells/PostCellViewController.swift
@@ -6,22 +6,42 @@
 //  Copyright © 2018 JoshTaylorKardos. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
 class PostCellViewController: UITableViewCell {
 	
-	
-	@IBOutlet weak var cellImage: UIImageView!
-	@IBOutlet weak var usernameLabel: UILabel!
-	@IBOutlet weak var cellLabel: UILabel!
-	
-	override func awakeFromNib() {
-		super.awakeFromNib()
-	}
-	
-	override func setSelected(_ selected: Bool, animated: Bool) {
-		super.setSelected(selected, animated: animated)
-	}
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var timeAgoLabel: UILabel!
+    @IBOutlet weak var universalIcon: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var postStatsLabel: UILabel!
+    @IBOutlet weak var likesButton: UIButton!
+    @IBOutlet weak var commentButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
+    
+    var post: Post!{
+        didSet {
+            self.updateUI()
+        }
+    }
+    
+    func updateUI(){
+        //profileImage.image = post
+    }
+    
+    
+    //    @IBOutlet weak var cellImage: UIImageView!
+//    @IBOutlet weak var usernameLabel: UILabel!
+//    @IBOutlet weak var cellLabel: UILabel!
+//    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//    }
+//    
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//    }
 	
 }
